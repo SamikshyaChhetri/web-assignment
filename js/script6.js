a6b73a86264a03453c34e051970cbdc015f1ce6c
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-14 20:16:51 +0545

    gave bg color to input text

--- js/script6.js
+++ js/script6.js
@@ -4,5 +4,6 @@ function myfunction() {
   const element = document.getElementById("display");
   const inpt = document.getElementById("input");
   const ip_value = inpt.value;
-  element.innerHTML = "This is my text" + " " + ip_value;
+  element.innerHTML =
+    "This is my text" + " " + "<span class='style'>" + ip_value + "</span>";
 }
